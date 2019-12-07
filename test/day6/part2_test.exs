012629a59f7a67c025a1debcf2b3c39661401587
Taylor O'Brien <obrien.taylorm@gmail.com>
2019-12-06 21:16:06 -0600

    day6 part 2

---
+++ test/day6/part2_test.exs
@@ -0,0 +1,24 @@
+defmodule AOC.Day6.Part2.Test do
+  use ExUnit.Case
+  alias AOC.Day6.Part2
+
+  test "example" do
+    input = ~w[
+      COM)B
+      B)C
+      C)D
+      D)E
+      E)F
+      B)G
+      G)H
+      D)I
+      E)J
+      J)K
+      K)L
+      K)YOU
+      I)SAN
+    ]
+
+    assert 4 == Part2.run(input)
+  end
+end
--- test/day6_test.exs
+++ test/day6_test.exs
@@ -5,4 +5,8 @@ defmodule AOC.Day6.Test do
   test "part1" do
     assert 145250 == Day6.part1()
   end
+
+  test "part2" do
+    assert 274 == Day6.part2()
+  end
 end
