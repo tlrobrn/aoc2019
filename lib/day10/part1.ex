db28e5d9a550fab78176b8af9f7b299410f4a76a
Taylor O'Brien <obrien.taylorm@gmail.com>
2019-12-10 17:22:16 -0600

    simplify 10 1

--- lib/day10/part1.ex
+++ lib/day10/part1.ex
@@ -2,32 +2,25 @@ defmodule AOC.Day10.Part1 do
   def run(input) do
     input
     |> Stream.map(&lines_for_point(&1, input))
-    |> Stream.map(&length/1)
     |> Enum.max()
   end
 
-  defp lines_for_point({x0, y0} = p, points) do
+  defp lines_for_point(p, points) do
     points
     |> Stream.reject(&(&1 == p))
-    |> Enum.reduce(%{}, fn {x1, y1} = q, visible ->
-      d1 = :math.sqrt((x1 - x0) * (x1 - x0) + (y1 - y0) * (y1 - y0))
-      visible
-      |> Enum.find(fn {{x2, y2}, _} ->
-        ((y1 - y0) * (x2 - x1) == (y2 - y1) * (x1 - x0)) and
-          ((x2 - x0) * (x1 - x0) >= 0) and
-          ((y2 - y0) * (y1 - y0) >= 0)
-      end)
-      |> case do
-        nil ->
-          Map.put_new(visible, q, d1)
-        {_, d2} when d2 < d1 ->
-          visible
-        {w, _} ->
-          visible
-          |> Map.delete(w)
-          |> Map.put_new(q, d1)
-      end
+    |> Enum.reduce(MapSet.new(), fn q, visible ->
+      MapSet.put(visible, angle(p, q))
     end)
-    |> Map.keys()
+    |> MapSet.size()
   end
+
+  defp angle({x0, y0}, {x1, y1}) do
+    case angle_against_vertical({x1 - x0, y1 - y0}) do
+      r when r < 0 -> 2 * :math.pi() - r
+      r -> r
+    end
+  end
+  defp angle_against_vertical(vector), do: :math.atan2(determinant(vector, {0, 1}), dot_product(vector, {0, 1}))
+  defp dot_product({x0, y0}, {x1, y1}), do: x0 * x1 + y0 * y1
+  defp determinant({x0, y0}, {x1, y1}), do: x0 * y1 - y0 * x1
 end
