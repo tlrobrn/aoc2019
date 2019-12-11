e2c330ff148963f48679e55b3da1f8dfcfe6235e
Taylor O'Brien <obrien.taylorm@gmail.com>
2019-12-11 10:20:03 -0600

    day 11 part 1

---
+++ lib/day11.ex
@@ -0,0 +1,4 @@
+defmodule AOC.Day11 do
+  use AOC.Intcode
+  use AOC.Day
+end
---
+++ test/day11_test.exs
@@ -0,0 +1,8 @@
+defmodule AOC.Day11.Test do
+  use ExUnit.Case, async: true
+  alias AOC.Day11
+
+  test "part 1" do
+    assert 2172 == Day11.part1()
+  end
+end
